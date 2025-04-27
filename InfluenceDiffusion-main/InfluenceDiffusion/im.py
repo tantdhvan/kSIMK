f9d4ac3be681a2fcaa20497a42a89476b7a0d122
Tan D. Tran <tantd.hvan@gmail.com>
2025-04-27 09:55:56 +0700

    update

--- InfluenceDiffusion-main/InfluenceDiffusion/im.py
+++ InfluenceDiffusion-main/InfluenceDiffusion/im.py
@@ -55,7 +55,7 @@ def single_simulation(model, seed_set_with_topics):
 def estimate_influence(model,seed_set_with_topics:dict[int,int]):
 
     assert isinstance(seed_set_with_topics, dict), "estimate_influence: seed_set_with_topics should always be a dictionary after update."
-    num_simulations=100
+    num_simulations=1000
     with Pool(processes=cpu_count()) as pool:
         results = pool.starmap(single_simulation, [(model,seed_set_with_topics)] * num_simulations)
 
@@ -93,7 +93,6 @@ def greedy_influence(model,b,n_topics=3):
         current_f=max_f
         seed_set_with_topics[max_node]=max_topic
         seed_set_weights[max_topic] += model.g.nodes[max_node]['weight']
-        print(seed_set_with_topics,seed_set_weights)
     end_time=time.time()
     return current_f,count_f,round(end_time-start_time,1)
 def get_Emax(model,node):
@@ -101,20 +100,20 @@ def get_Emax(model,node):
     tmp_f = estimate_influence(model,tmp_seed_set_with_topics)
     return tmp_f
 def get_threshold(M, epsilon, b, weight, n_topics):
+    upperB=M * b * n_topics/weight
     threshold = {}
     j = -1
     while True:
         j = j + 1
         tmp_threshold = (1 + epsilon) ** j
         if tmp_threshold > M * b * n_topics/weight:
-            break  # kiểm tra kết thúc trước
+            break 
         if tmp_threshold >= M:
-            threshold[j] = tmp_threshold  # lưu nếu thỏa điều kiện
+            threshold[j] = tmp_threshold
     return threshold    
-def streaming(model,node_weights,b,epsilon,alpha,n_topics=3):
+def streaming(model,b,epsilon,alpha,n_topics=3):
     start_time=time.time()
     M=0.0
-    seed_sets={}
     seed_sets_with_topics={}
     seed_sets_weights={}
     seed_sets_current_f={}
@@ -122,23 +121,26 @@ def streaming(model,node_weights,b,epsilon,alpha,n_topics=3):
     hash_f={}
     count_f=0
     f_emax=0
-    node_max=-1
+    node_emax=-1
     topic_emax=-1
     for node in g.nodes:
-        tmp_f=get_Emax(model,node)
-        count_f=count_f+1
-        for topic in range(n_topics):           
+        if g.nodes[node]['weight']>b:
+            continue
+        for topic in range(n_topics):
+            tmp_seed_set_with_topics={}
+            tmp_seed_set_with_topics[node]=topic
+            tmp_f = estimate_influence(model,tmp_seed_set_with_topics)
+            count_f=count_f+1
             tmp_seed={(node,topic)}
             hash_key=hash(frozenset(tmp_seed))
             hash_f[hash_key]=tmp_f
-            if(tmp_f/node_weights[node][topic]>f_emax):
-                f_emax=tmp_f/node_weights[node][topic]
-                node_max=node
+            if(tmp_f>f_emax):
+                f_emax=tmp_f
+                node_emax=node
                 topic_emax=topic
                 M=tmp_f
-    threshold=get_threshold(M,epsilon,b,node_weights[node_max][topic_emax],n_topics)
+    threshold=get_threshold(M,epsilon,b,model.g.nodes[node_emax]['weight'],n_topics)
     for j in threshold.keys():
-        seed_sets[j]=[]
         seed_sets_with_topics[j]={}
         seed_sets_current_f[j]=0.0
         seed_sets_weights[j]={}
@@ -149,59 +151,51 @@ def streaming(model,node_weights,b,epsilon,alpha,n_topics=3):
     for node in g.nodes:
         for j, threshold_j in sorted(threshold.items()):              
             for topic in range(n_topics):
-                if(seed_sets_weights[j][topic] + node_weights[node][topic] > b):
+                if(seed_sets_weights[j][topic] + model.g.nodes[node]['weight'] > b):
                     continue
-                tmp_seed=deepcopy(seed_sets[j])
                 tmp_seed_set_with_topics=deepcopy(seed_sets_with_topics[j])
                 tmp_seed_topics=deepcopy(seed_topics[j])
-
-                tmp_seed.append(node)
                 tmp_seed_set_with_topics[node]=topic
                 tmp_seed_topics.append((node,topic))
                 tmp_hash=hash(frozenset(tmp_seed_topics))
                 if tmp_hash in hash_f:
                     tmp_f=hash_f[tmp_hash]
                 else:
-                    tmp_f = estimate_influence(model, tmp_seed,tmp_seed_set_with_topics)
+                    tmp_f = estimate_influence(model,tmp_seed_set_with_topics)
                     count_f=count_f+1
                     hash_f[tmp_hash]=tmp_f
 
-                tmp_delta = (tmp_f - seed_sets_current_f[j])/node_weights[node][topic]
+                tmp_delta = (tmp_f - seed_sets_current_f[j])/model.g.nodes[node]['weight']
                 if tmp_delta > threshold_j*alpha/b:
-                    seed_sets[j]=tmp_seed
                     seed_sets_with_topics[j]=tmp_seed_set_with_topics
-                    seed_sets_weights[j][topic]=seed_sets_weights[j][topic]+node_weights[node][topic]
+                    seed_sets_weights[j][topic]=seed_sets_weights[j][topic]+model.g.nodes[node]['weight']
                     seed_sets_current_f[j]=tmp_f
                     seed_topics[j]=tmp_seed_topics
     
     for node in g.nodes:
         for j,threshold_j in sorted(threshold.items()):
-            if node in seed_sets[j]:
+            if node in seed_sets_with_topics[j].keys():
                 continue
             for topic in range(n_topics):
-                if(seed_sets_weights[j][topic] + node_weights[node][topic] > b):
+                if(seed_sets_weights[j][topic] + model.g.nodes[node]['weight'] > b):
                     continue
-                tmp_seed=deepcopy(seed_sets[j])
                 tmp_seed_set_with_topics=deepcopy(seed_sets_with_topics[j])
                 tmp_seed_topics=deepcopy(seed_topics[j])
 
-                tmp_seed.append(node)
                 tmp_seed_set_with_topics[node]=topic
                 tmp_seed_topics.append((node,topic))
-
 
                 tmp_hash=frozenset(tmp_seed_topics)
                 if tmp_hash in hash_f:
                     tmp_f=hash_f[tmp_hash]
                 else:
-                    tmp_f = estimate_influence(model, tmp_seed,tmp_seed_set_with_topics)
+                    tmp_f = estimate_influence(model,tmp_seed_set_with_topics)
                     count_f=count_f+1
                     hash_f[tmp_hash]=tmp_f
 
                 if tmp_f>seed_sets_current_f[j]:
-                    seed_sets[j]=tmp_seed
                     seed_sets_with_topics[j]=tmp_seed_set_with_topics
-                    seed_sets_weights[j][topic]=seed_sets_weights[j][topic]+node_weights[node][topic]
+                    seed_sets_weights[j][topic]=seed_sets_weights[j][topic]+model.g.nodes[node]['weight']
                     seed_sets_current_f[j]=tmp_f
                     seed_topics[j]=tmp_seed_topics
     end_time=time.time()
@@ -212,24 +206,13 @@ filename= 'graph_'+str(n_nodes)+'_'+str(p)+'.pkl'
 g = get_or_create_graph(filename,n_nodes=n_nodes,p=p)
 n_topics=3
 epsilon=0.2
-B=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
+B=[1.0,2.0,3.0,4.0,5.0]
 alpha=0.2
 modelICM = kICM(g,n_topics=n_topics)
-'''
-seed_set_with_topics: dict[int, int] = {}
-seed_set_with_topics[0]=0
-seed_set_with_topics[1]=1
-seed_set_with_topics[2]=2
-seed_set_with_topics[400]=0
 
-print('S: ',seed_set_with_topics)
-
-f=modelICM.sample_trace(seed_set_with_topics)
-print(f)
-'''
-#for b in B:
-    #f_str,count_f_str,time_str=streaming(modelICM,node_weights,b,epsilon,alpha,n_topics=n_topics)
-    #print('Streaming,',b,',',f_str,',',count_f_str,',',time_str)
-f_greedy,count_f_greedy,time_greedy=greedy_influence(modelICM,b=1.0,n_topics=n_topics)
-print('Greedy,',1.0,',',f_greedy,',',count_f_greedy,',',time_greedy)
+for b in B:
+    f_str,count_f_str,time_str=streaming(modelICM,b,epsilon,alpha,n_topics=n_topics)
+    print('Streaming,',b,',',f_str,',',count_f_str,',',time_str)
+    f_greedy,count_f_greedy,time_greedy=greedy_influence(modelICM,b,n_topics=n_topics)
+    print('Greedy,',b,',',f_greedy,',',count_f_greedy,',',time_greedy)
 
